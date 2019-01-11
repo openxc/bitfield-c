3e1746adbc354d59d2a9b89b9122e313e4d95c40
Anton Gerasimov <anton.gerasimov@here.com>
2019-01-11 16:43:43 +0100

    Fix deprecated GNU designators

--- src/bitfield/bitfield.c
+++ src/bitfield/bitfield.c
@@ -60,7 +60,7 @@ bool set_bitfield(const uint64_t value, const uint16_t offset,
     }
 
     ArrayOrBytes combined = {
-        whole: value
+        .whole = value
     };
 
 #ifdef BYTE_ORDER_LITTLE
